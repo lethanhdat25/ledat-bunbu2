c08a4128f73a53b007d0c8e5ac3e5b982f74b855
isaacbest.ig@gmail.com <isaacbest.ig@gmail.com>
2021-12-07 09:23:24 +0700

    backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
 const express=require("express");
 const app=express();
-const port = 8080;
+const port=8080;
 const cors=require("cors");
 
+//Use CORS
 app.use(cors());
-app.get("/",(req, res) =>{
-    res.send("HELLO")
-});
-app.listen(port,()=>console.log("SERVER IS RUNNING..."))
+
+app.get("/",((req, res) => {
+    const data={
+        colors : [
+            "violet", "cornflowerblue", "gold", "orange",
+            "turquoise", "tomato", "greenyellow"
+        ],
+        symbols : [
+            "circle", "star", "square", "triangleUp",
+            "triangleDown", "diamond", "plus"
+        ]
+    }
+    return res.json(data);
+}))
+app.listen(port,()=>console.log("SERVER IS RUNNING..."));
