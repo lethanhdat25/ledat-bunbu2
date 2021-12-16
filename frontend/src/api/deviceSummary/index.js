4a19c7178c89888966019f97a3f3224219a40a6d
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-12-16 17:41:37 +0700

    Device Summary

---
+++ frontend/src/api/deviceSummary/index.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export const deviceSummaryApi = {
+    getDeviceSummary: async () => {
+        return await axios.get("http://localhost:8080/device_summary");
+    }
+}
---
+++ frontend/src/store/index.js
@@ -0,0 +1,9 @@
+import {configureStore} from "@reduxjs/toolkit";
+import rootReducer from "./slice";
+import thunk from "redux-thunk";
+
+const store=configureStore({
+    reducer:rootReducer,
+    middleware:[thunk]
+})
+export  default store;
