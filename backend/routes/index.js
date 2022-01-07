de127b3677a564c9ee148246b56c821cbcbb66b3
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2022-01-07 09:19:59 +0700

    up code device by hour

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,5 @@
 const victoryRouter=require("./victory");
 const deviceSummaryRouter=require("./deviceSummary");
-const deviceTypeRouter=require("./deviceType");
 const deviceRouter=require("./device");
 const rankingRouter=require("./ranking");
 const deviceByHourRouter=require("./deviceByHour");
@@ -8,7 +7,6 @@ const deviceByHourRouter=require("./deviceByHour");
 function route(app){
     app.use("/victory",victoryRouter);
     app.use("/device_summary",deviceSummaryRouter);
-    app.use("/device_type",deviceTypeRouter);
     app.use("/device",deviceRouter);
     app.use("/ranking",rankingRouter);
     app.use("/device_by_hour",deviceByHourRouter);
