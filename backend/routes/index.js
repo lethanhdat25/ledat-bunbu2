7d2206f1b1ee4fdfc4cb09716f5feebc8931c559
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2022-01-07 09:59:36 +0700

    up code device by hour

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,11 @@
 const victoryRouter=require("./victory");
 const deviceSummaryRouter=require("./deviceSummary");
-const deviceRouter=require("./device");
 const rankingRouter=require("./ranking");
 const deviceByHourRouter=require("./deviceByHour");
 
 function route(app){
     app.use("/victory",victoryRouter);
     app.use("/device_summary",deviceSummaryRouter);
-    app.use("/device",deviceRouter);
     app.use("/ranking",rankingRouter);
     app.use("/device_by_hour",deviceByHourRouter);
 }
--- frontend/src/store/slice/index.js
+++ frontend/src/store/slice/index.js
@@ -1,16 +1,7 @@
 import {combineReducers} from "redux";
-import deviceType from "./device_type";
-import device from "./device";
 import ranking from "./ranking";
-import deviceSummary from "./device_summary";
-import label_pie_chart from "./label_pie_chart";
 import device_by_hour from "./device_by_hour";
-
 export const rootReducer=combineReducers({
-    deviceType:deviceType,
-    device:device,
-    deviceSummary:deviceSummary,
     ranking:ranking,
-    label_pie_chart:label_pie_chart,
     device_by_hour:device_by_hour
 });
