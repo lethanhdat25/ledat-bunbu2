0174450e683cf4b189c85a51fe28892c17615492
lethanhdat25 <isaacbest.ig@gmail.com>
2022-02-15 08:51:01 +0700

    done

--- backend/routes/deviceSummary.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router=express.Router();
-
-router.get('/',(req, res) =>{
-    setTimeout(()=>res.json( {ios: 40, android: 60}),10000);
-})
-
-export default router;
